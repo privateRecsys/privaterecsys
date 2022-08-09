97641f4368a00181bf69bf329c6f60a4e1e0a4e4
Privax <mariaterzi@Marias-MacBook-Pro.local>
2022-08-09 14:28:55 +0300

    differetial privacy algorithms, data pre-processing and evaluation

--- pythonProject/main.py
+++ pythonProject/main.py
@@ -1,7 +1,8 @@
-# This is a sample Python script.
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+from diffprivacy.MF_recommender import Matrix_Factorization
+from eval import evaluator
+
+import pandas as pd
 
 
 def print_hi(name):
@@ -11,6 +12,14 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    print_hi('Recsys')
+    train = pd.read_csv('dataset/ml-100k/user-rating.csv', index_col=0)
+    test = pd.read_csv('dataset/ml-100k/user-rating_test.csv', index_col=0)
+    MF_estimate = Matrix_Factorization.Matrix_Factorization(K=3, epoch=10,beta=0.06)
+    MF_estimate.fit(train)
+    R_hat = MF_estimate.start()
+    non_index = test.values.nonzero()
+    pred_MF = R_hat[non_index[0], non_index[1]]
+    actual = test.values[non_index[0], non_index[1]]
+    print('MSE of MF is %s' % evaluator.comp_mse(pred_MF, actual))
+    print('RMSE of MF is %s' % evaluator.comp_rmse(pred_MF, actual))
