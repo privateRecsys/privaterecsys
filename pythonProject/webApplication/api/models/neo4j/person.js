114f49989bd07e896983e159b74e2c04fa9af713
Privax <mariaterzi@Marias-MacBook-Pro.local>
2022-08-19 14:13:01 +0300

    first API

--- pythonProject/webApplication/api/models/neo4j/person.js
+++
@@ -1,9 +0,0 @@
-// extracts just the data from the query results
-
-const _ = require('lodash');
-
-const Person = module.exports = function (_node) {
-  _.extend(this, _node.properties);
-  this.id = this.tmdbId;
-  this.poster_image = this.poster;
-};
