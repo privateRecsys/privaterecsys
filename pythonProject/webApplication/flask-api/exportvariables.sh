c091d10b2091acf64a4b2a338b6e1e1440d545cb
privX <privaterecsys@gmail.com>
2022-09-03 14:34:11 +0300

    ui

--- pythonProject/webApplication/flask-api/exportvariables.sh
+++ pythonProject/webApplication/flask-api/exportvariables.sh
@@ -1,4 +1,4 @@
 export SECRET_KEY="super secret guy"
 export MOVIE_DATABASE_USERNAME="neo4j"
-export MOVIE_DATABASE_PASSWORD="password"
-export MOVIE_DATABASE_URL="bolt://localhost:7687"
+export MOVIE_DATABASE_PASSWORD="bread-chance-coal"
+export MOVIE_DATABASE_URL="bolt://3.239.25.108:7687"
