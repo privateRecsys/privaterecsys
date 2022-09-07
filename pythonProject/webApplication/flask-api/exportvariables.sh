10f350b194d987721e369df2ce123a4d52e1a9c4
privX <privaterecsys@gmail.com>
2022-09-07 09:47:44 +0300

    fixing problem with localhost, and script for loading data locally

--- pythonProject/webApplication/flask-api/exportvariables.sh
+++ pythonProject/webApplication/flask-api/exportvariables.sh
@@ -1,4 +1,4 @@
 export SECRET_KEY="super secret guy"
 export MOVIE_DATABASE_USERNAME="neo4j"
-export MOVIE_DATABASE_PASSWORD="bread-chance-coal"
-export MOVIE_DATABASE_URL="bolt://3.239.25.108:7687"
+export MOVIE_DATABASE_PASSWORD="neo4j"
+export MOVIE_DATABASE_URL="neo4j://localhost:7687"
