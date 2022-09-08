04118973865968f4c083c60d214235474dbb9f41
privX <privaterecsys@gmail.com>
2022-09-08 17:15:03 +0300

    searx work in progress

--- pythonProject/webApplication/flask-api/exportvariables.sh
+++ pythonProject/webApplication/flask-api/exportvariables.sh
@@ -1,4 +1,4 @@
 export SECRET_KEY="super secret guy"
 export MOVIE_DATABASE_USERNAME="neo4j"
-export MOVIE_DATABASE_PASSWORD="neo4j"
-export MOVIE_DATABASE_URL="neo4j://localhost:7687"
+export MOVIE_DATABASE_PASSWORD="dishes-hitches-blanket"
+export MOVIE_DATABASE_URL="bolt://18.207.210.61:7687"
