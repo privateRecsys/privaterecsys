0bcdde39ee8c05a955e69c6c5bfa4025469f34d3
privX <privaterecsys@gmail.com>
2022-10-31 23:47:21 +0200

    docker files

--- pythonProject/webApplication/flask-api/exportvariables.sh
+++ pythonProject/webApplication/flask-api/exportvariables.sh
@@ -1,4 +1,4 @@
 export SECRET_KEY="super secret guy"
 export MOVIE_DATABASE_USERNAME="neo4j"
-export MOVIE_DATABASE_PASSWORD="mover-policy-world"
-export MOVIE_DATABASE_URL="bolt://3.238.76.5:7687"
+export MOVIE_DATABASE_PASSWORD="probe-tears-independence"
+export MOVIE_DATABASE_URL="bolt://18.214.39.110:7687"
